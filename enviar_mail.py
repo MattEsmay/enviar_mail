07d3ecc84e536bb516ed68a2ff4165fd168d0c40
Matías Esmay <matiasesmay@gmail.com>
2023-05-16 20:19:13 -0300

    Update enviar_mail.py

--- enviar_mail.py
+++ enviar_mail.py
@@ -1,2 +1,34 @@
+import os
+import smtplib
+from email.message import EmailMessage
+
+user_gmail = os.getenv("USER_MAIL")
+password_gmail = os.getenv("PASSWORD_MAIL")
+
+def send_notifying_mail(mail_user: str = "", mail_password: str = "") -> None:
+    # aqui va tu codigo
+
+    msg = EmailMessage()
+
+    # Contenido
+    msg['From']="nuevo.espacio.lab@gmail.com"
+    msg['To']="matiasesmay@gmail.com"
+    msg['Subject']= "Probando mandar mails!"
+    cuerpo_del_mail = 'Este es un mail enviado con Python en la clase! =D'
+    msg.set_content(cuerpo_del_mail)
+
+    # No se queden en los detalles aquí, pero pueden leer más sobre el protocolo SMTP acá: https://es.wikipedia.org/wiki/Protocolo_para_transferencia_simple_de_correo 
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+
+    # Usuario y contraseña
+    usuario = 'yrogbyiupdfknotw'
+
+    server.login(usuario, password)
+
+    # enviar
+    server.send_message(msg)
+    server.quit();
+
 if __name__ == "__main__":
-      enviar_mail(USER_GMAIL, PASSWORD_GMAIL) # modificar segun se llame tu funcion y parametros
+      send_notifying_mail(USER_GMAIL, PASSWORD_GMAIL) # modificar segun se llame tu funcion y parametros
