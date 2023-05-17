b4a8d366e38eafb17ad97b850c81451b65ca9e46
Matías Esmay <matiasesmay@gmail.com>
2023-05-16 21:13:08 -0300

    Update enviar_mail.py

--- enviar_mail.py
+++ enviar_mail.py
@@ -2,8 +2,8 @@ import os
 import smtplib
 from email.message import EmailMessage
 
-user_gmail = os.getenv("USER_MAIL")
-password_gmail = os.getenv("PASSWORD_MAIL")
+user_gmail = os.getenv("USER_GMAIL")
+password_gmail = os.getenv("PASSWORD_GMAIL")
 
 def send_notifying_mail(mail_user: str = "", mail_password: str = "") -> None:
     # aqui va tu codigo
@@ -22,13 +22,16 @@ def send_notifying_mail(mail_user: str = "", mail_password: str = "") -> None:
     server.starttls()
 
     # Usuario y contraseña
-    usuario = 'yrogbyiupdfknotw'
+    # usuario = ''
 
-    server.login(usuario, password)
+    server.login(user_gmail, password_gmail)
 
     # enviar
     server.send_message(msg)
     server.quit();
 
+#    send_notifying_mail(user_gmail, password_gmail)
+
 if __name__ == "__main__":
-      send_notifying_mail(USER_GMAIL, PASSWORD_GMAIL) # modificar segun se llame tu funcion y parametros
+      send_notifying_mail(user_gmail, password_gmail) # modificar segun se llame tu funcion y parametros
+    
